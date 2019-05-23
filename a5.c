1b7a70d13bba6a2af1229f5e01d2ea9fe111c68f
aravintharajp <50869424+aravintharajp@users.noreply.github.com>
2019-05-24 05:26:39 +0530

    Update and rename README.md to a5.c

---
+++ a5.c
@@ -0,0 +1,21 @@
+
+int main()
+{
+   
+int n,m,o;
+   
+scanf("%d%d%d",&n,&m,&o);
+   
+if(n>=m&&n>=o)
+   
+printf("%d",n);
+   
+else if(m>=n&&m>=o)
+   
+printf("%d",m);
+   
+else
+   
+printf("%d",o);
+
+}
